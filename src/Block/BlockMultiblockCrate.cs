c066ea260d397c3e2f4bf1c66ca560a6ff077ab0
Craluminum2413 <beware.14b@gmail.com>
2022-05-18 21:03:48 +0300

    Implemented more flexible localization

--- src/Block/BlockMultiblockCrate.cs
+++ src/Block/BlockMultiblockCrate.cs
@@ -1,4 +1,5 @@
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
@@ -24,6 +25,39 @@ namespace MultiblockCrates.Content
 				itemStack.Attributes.SetString("type", this.Props.DefaultType);
 			}
 			return itemStack;
+		}
+
+		public override string GetHeldItemName(ItemStack itemStack)
+		{
+			string @string = itemStack.Attributes.GetString("type", this.Props.DefaultType);
+			string text = itemStack.Attributes.GetString("lidState", "closed");
+			if (text.Length == 0)
+			{
+				text = "closed";
+			}
+			string[] array = new string[5];
+			int num = 0;
+			AssetLocation code = this.Code;
+			array[num] = ((code != null) ? code.Domain : null);
+			array[1] = ":block-";
+			array[2] = @string;
+			array[3] = "-";
+			int num2 = 4;
+			AssetLocation code2 = this.Code;
+			array[num2] = ((code2 != null) ? code2.Path : null);
+			return Lang.GetMatching(string.Concat(array), new object[]
+			{
+				Lang.Get("cratelidstate-" + text, new object[]
+				{
+					"closed"
+				}
+				),
+				Lang.Get("mcrate:cratewood-" + @string, new object[]
+				{
+					"wood"
+				}
+				)
+			});
 		}
   }
 }
